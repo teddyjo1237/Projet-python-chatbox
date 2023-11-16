4b0c4dc19e2a3b1b1496eefc9e78479df0e6142f
AATwiZz <eliot.viard@gmail.com>
2023-11-16 14:55:52 +0100

    Creating function to run through the list of files

--- functions.py
+++ functions.py
@@ -1,1 +1,9 @@
+import os
 
+
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
