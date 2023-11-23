de710137c6727a96305ec7c3ae9e028e09c71adc
teddyjo1237 <125256084+teddyjo1237@users.noreply.github.com>
2023-11-23 14:50:47 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,5 @@
-'''
-- Extract the names of the presidents from the names of the text files provided ;
-- Associate a first name with each president;
-- Display the list of president's names (watch out for duplicates) ;
-- Convert the texts in the 8 files to lower case and store the contents in new files. The new files are to be
-stored in a new folder called "cleaned". This folder should be located in the main directory of the main.py
-program, and at the same level as the "speeches" directory.
-- For each file stored in the "cleaned" directory, run through its text and remove any punctuation characters.
+
+'''- For each file stored in the "cleaned" directory, run through its text and remove any punctuation characters.
 The final result should be a file with words separated by spaces. Please note that some characters, such as
 the apostrophe (') or the dash (-), requires special treatment to avoid concatenating two words (e.g. "ellemême" should become "elle même" and not "ellemême"). Changes made at this phase should be stored
 in the same files in the "cleaned" directory.'''
